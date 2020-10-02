3b1956531a1c80d3c089c154751657e881c19eed
Victor Sklyar <vinnitu@gmail.com>
2020-10-02 10:52:00 +0300

    Update douglas-peucker.js

--- douglas-peucker.js
+++ douglas-peucker.js
@@ -2,27 +2,32 @@
 function getSqSegDist(p, p1, p2) {
     var x = p1[0],
         y = p1[1],
+        z = p1[2],
         dx = p2[0] - x,
-        dy = p2[1] - y;
+        dy = p2[1] - y,
+        dz = p2[2] - z;
 
-    if (dx !== 0 || dy !== 0) {
+    if (dx !== 0 || dy !== 0 || dz !== 0) {
 
-        var t = ((p[0] - x) * dx + (p[1] - y) * dy) / (dx * dx + dy * dy);
+        var t = ((p[0] - x) * dx + (p[1] - y) * dy + (p[2] - z) * dz) / (dx * dx + dy * dy + dz * dz);
 
         if (t > 1) {
             x = p2[0];
             y = p2[1];
+            z = p2[2];
 
         } else if (t > 0) {
             x += dx * t;
             y += dy * t;
+            z += dz * t;
         }
     }
 
     dx = p[0] - x;
     dy = p[1] - y;
+    dz = p[2] - z;
 
-    return dx * dx + dy * dy;
+    return dx * dx + dy * dy + dz * dz;
 }
 
 function simplifyDPStep(points, first, last, sqTolerance, simplified) {
